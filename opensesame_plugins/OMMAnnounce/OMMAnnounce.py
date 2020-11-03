a14d75c46da9f41987b05934b70db111bc538a92
smathot <s.mathot@cogsci.nl>
2020-11-03 16:38:40 +0100

    Support all backends

--- opensesame_plugins/OMMAnnounce/OMMAnnounce.py
+++ opensesame_plugins/OMMAnnounce/OMMAnnounce.py
@@ -19,19 +19,40 @@ class OMMAnnounce(BaseOMMPlugin, Item):
     def run(self):
         
         # We dynamically set the custom log backend module so that it's
-        # automatically found by OpenSesame
+        # automatically found by OpenSesame.
         from openmonkeymind import _omm_log_backend
         sys.modules['openexp._log.omm'] = _omm_log_backend
-
+        # Get the experiment and patch it so that re-uses the environment of
+        # the current experiment, i.e. it doesn't create its own window etc.
         exp = self._openmonkeymind.announce(self.var.omm_participant)
-        exp.var.log_backend = 'omm'
         item_stack.item_stack_singleton.clear = lambda: None
         exp.init_display = lambda: None
         exp.end = lambda: None
-        if hasattr(self.experiment, 'window'):
-            exp.window = self.experiment.window
-            exp.python_workspace['win'] = self.experiment.window
+        exp.window = self.experiment.window
+        exp.python_workspace['win'] = self.experiment.window
         exp.python_workspace['omm'] = self._openmonkeymind
+        # A few back-end-specific properties need to be copied to the
+        # experiment.
+        if self.experiment.var.canvas_backend == 'xpyriment':
+            exp.expyriment = self.experiment.expyriment
+        elif self.experiment.var.canvas_backend == 'legacy':
+            exp.surface = self.experiment.surface
+        # The backend settings need to be copied as well, although the log 
+        # backend is always set to omm.
+        exp.var.log_backend = 'omm'
+        for backend in [
+            'canvas_backend',
+            'keyboard_backend',
+            'mouse_backend',
+            'sampler_backend',
+            'clock_backend',
+            'color_backend'
+        ]:
+            if backend in exp.var:
+                if backend in self.experiment.var:
+                    exp.var.set(backend, self.experiment.var.get(backend))
+                else:
+                    exp.var.__delattr__(backend)
         exp.run()
 
 
