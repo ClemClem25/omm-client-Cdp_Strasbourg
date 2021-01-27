c10c892961069a64a39ad618fccc5d0e41159f29
smathot <s.mathot@cogsci.nl>
2021-01-27 13:55:37 +0100

    Bump to 0.2.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='opensesame-extension-omm',
-    version='0.2.2',
+    version='0.2.3',
     description='OpenMonkeyMind plugins and extension for OpenSesame',
     author='Sebastiaan Mathot',
     author_email='s.mathot@cogsci.nl',
