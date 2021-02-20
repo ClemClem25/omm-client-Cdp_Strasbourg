a73e4611b852fa885b8e3fca705626a8257e4a3c
smathot <s.mathot@cogsci.nl>
2021-02-20 17:20:44 +0100

    Check server healthz asynchronously

--- opensesame_extensions/OpenMonkeyMind/OpenMonkeyMind.py
+++ opensesame_extensions/OpenMonkeyMind/OpenMonkeyMind.py
@@ -5,12 +5,19 @@ import os
 import tempfile
 import textwrap
 import yaml
+import multiprocessing
 from qtpy.QtWidgets import QFileDialog
+from qtpy.QtCore import QTimer
 from libqtopensesame.misc.config import cfg
 from libqtopensesame.extensions import BaseExtension
 from libqtopensesame.misc.translate import translation_context
 from openmonkeymind import OpenMonkeyMind as OMM
 _ = translation_context(u'OpenMonkeyMind', category=u'extension')
+
+
+def server_ping(queue, server, port):
+    
+    queue.put(OMM(server, port).available)
 
 
 class OpenMonkeyMind(BaseExtension):
@@ -77,17 +84,40 @@ class OpenMonkeyMind(BaseExtension):
             return
         cfg.omm_local_logfile = path
         self._w.ui.cfg_omm_local_logfile.setText(path)
-
-    def _validate(self):
         
-        self._w.ui.button_start.setEnabled(True)
-        if OMM(server=cfg.omm_server, port=cfg.omm_port).available:
+    def _start_server_ping(self):
+        
+        self._queue = multiprocessing.Queue()
+        self._server_ping = multiprocessing.Process(
+            target=server_ping,
+            args=(self._queue, cfg.omm_server, cfg.omm_port)
+        )
+        self._server_ping.start()
+        self.extension_manager.fire(
+            'register_subprocess',
+            pid=self._server_ping.pid,
+            description='openmonkey_mind_server_ping'
+        )
+        QTimer.singleShot(500, self._check_server_ping)
+        
+    def _check_server_ping(self):
+        
+        if self._queue.empty():
+            QTimer.singleShot(500, self._check_server_ping)
+            return
+        available = self._queue.get()
+        if available:
             self._w.ui.label_server_status.setText('✓')
             self._w.ui.label_server_status.setStyleSheet('color:green;')
         else:
             self._w.ui.label_server_status.setText('✕')
             self._w.ui.label_server_status.setStyleSheet('color:red;')
             self._w.ui.button_start.setEnabled(False)
+
+    def _validate(self):
+        
+        self._w.ui.button_start.setEnabled(True)
+        self._start_server_ping()
         try:
             yaml_data = yaml.safe_load(self._w.ui.omm_yaml_data.toPlainText())
         except:
