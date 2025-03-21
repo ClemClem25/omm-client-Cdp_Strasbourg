49697381cb819163846d5b661676a8d56610c011
Clément Yasar <147620717+ClemClem25@users.noreply.github.com>
2025-03-21 15:13:12 +0100

    Update __init__.py

--- opensesame_plugins/open_monkey_mind/omm_conditioner/conditioners/__init__.py
+++ opensesame_plugins/open_monkey_mind/omm_conditioner/conditioners/__init__.py
@@ -1,5 +1,5 @@
-from libopensesame.py3compat import *
 from ._seed_dispenser import SeedDispenser
 from ._dummy import Dummy
+from ._juice_pump_cdp import JuicePumpCdp
 
-__all__ = ['SeedDispenser', 'Dummy']
+__all__ = ['SeedDispenser', 'Dummy', 'JuicePumpCdp']
